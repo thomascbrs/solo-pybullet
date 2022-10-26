6700c706eb3de27d63549e16d5af2ad21df66ae2
thomasCbrs <thomas.corberes@gmail.com>
2022-10-26 12:15:04 +0100

    Modify main to run Caracal controller and log the data

--- solo_pybullet/__main__.py
+++ solo_pybullet/__main__.py
@@ -8,26 +8,33 @@ import time
 
 import pybullet as p  # PyBullet simulator
 
-from .controller import c_walking_IK  # Controller functions
+from solo_pybullet.controller import c_walking_IK, c_walking_ID  # Controller functions
+from solo_pybullet.controller_caracal import Controller
 # Functions to initialize the simulation and retrieve joints positions/velocities
-from .initialization_simulation import configure_simulation, getPosVelJoints
+from solo_pybullet.initialization_simulation import configure_simulation, getPosVelJoints, getContactForces
+from solo_pybullet.Logger import Logger
 
 ####################
 #  INITIALIZATION ##
 ####################
 
 dt = 0.001  # time step of the simulation
+dt_mpc = 0.02
+LOGGER = True
 # If True then we will sleep in the main loop to have a 1:1 ratio of (elapsed real time / elapsed time in the
 # simulation)
-realTimeSimulation = True
+realTimeSimulation = False
 enableGUI = True  # enable PyBullet GUI or not
 robotId, solo, revoluteJointIndices = configure_simulation(dt, enableGUI)
+
+controller = Controller(dt)
+logger = Logger(dt,dt_mpc)
 
 ###############
 #  MAIN LOOP ##
 ###############
 
-for i in range(10000):  # run the simulation during dt * i_max seconds (simulation time)
+for i in range(6000):  # run the simulation during dt * i_max seconds (simulation time)
 
     # Time at the start of the loop
     if realTimeSimulation:
@@ -37,8 +44,17 @@ for i in range(10000):  # run the simulation during dt * i_max seconds (simulati
     q, qdot = getPosVelJoints(robotId, revoluteJointIndices)
 
     # Call controller to get torques for all joints
-    jointTorques = c_walking_IK(q, qdot, dt, solo, i * dt)
+    jointTorques = controller.compute(q, qdot, dt, i)
 
+    if LOGGER:
+        logger.log(controller._results)
+        # if i > 4000:
+        # Get contact forces measured in Pybullet
+        f,s = getContactForces(robotId, revoluteJointIndices)
+        logger.log_pyb_forces(f)
+
+    # This resets the velocity controller for joints that are not actuated
+    p.setJointMotorControlArray(robotId, revoluteJointIndices,p.VELOCITY_CONTROL,forces=[0. for name in revoluteJointIndices])
     # Set control torques for all joints in PyBullet
     p.setJointMotorControlArray(robotId, revoluteJointIndices, controlMode=p.TORQUE_CONTROL, forces=jointTorques)
 
@@ -50,6 +66,8 @@ for i in range(10000):  # run the simulation during dt * i_max seconds (simulati
         t_sleep = dt - (time.clock() - t0)
         if t_sleep > 0:
             time.sleep(t_sleep)
-
 # Shut down the PyBullet client
 p.disconnect()
+if LOGGER:
+    logger.save()
+    logger.plot_all(2.,8.)
